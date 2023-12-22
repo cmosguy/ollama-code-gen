1feee27c99f9ea7ff6a119c87c0d5e72ccc8dd61
Adam Klein <adam@thinkcru.com>
2023-12-22 11:57:21 -0600

    first work in progress attempt to getting this working

---
+++ src/main.py
@@ -0,0 +1,54 @@
+### Code used:
+import autogen
+import debugpy
+
+
+debugpy.listen(5678)
+print("Waiting for debugger attach")
+debugpy.wait_for_client()
+if debugpy.is_client_connected():
+    print("Debugger attached")
+
+config_list_mistral = [
+    {
+        'base_url': "http://thinker:5000/v1",
+        'api_key': "NULL"
+    }
+]
+
+config_list_codellama = [
+    {
+        'base_url': "http://engineer:5000/v1",
+        'api_key': "NULL"
+    }
+]
+
+llm_config_mistral={
+    "config_list": config_list_mistral,
+}
+
+llm_config_codellama={
+    "config_list": config_list_codellama,
+}
+
+coder = autogen.AssistantAgent(
+    name="Coder",
+    llm_config=llm_config_codellama
+)
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={"work_dir": "web"},
+    llm_config=llm_config_mistral,
+    system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
+Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
+)
+
+task="""
+Write a python script to output numbers 1 to 100 and then the user_proxy agent should run the script
+"""
+
+user_proxy.initiate_chat(coder, message=task)
